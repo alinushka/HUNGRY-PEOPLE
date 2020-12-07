5cef8cdf9015c1da0449fc116981a190f3767078
alinushka <salatbulka@mail.ru>
2020-12-07 09:26:29 +0400

    START

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  var tabsItem = $(".tabs__item");
-  var contentItem = $(".content__item");
-
-  tabsItem.on("click", function (event) {
-    console.log($(this).attr("data-target"));
-  });
-});
